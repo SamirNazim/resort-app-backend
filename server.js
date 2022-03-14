de1a72782a484a5df9ae6946a4204e2d25c46431
Samir Nazim <samirn@Live.ca>
2022-03-14 18:48:03 -0400

    edited server.js

--- server.js
+++ server.js
@@ -8,10 +8,26 @@ const userController = require('./controllers/UserController.js')
 const propertyController = require('./controllers/PropertyController.js')
 
 
+
+// cors
+const allowlist = ['http://localhost:3000', 'https://serene-mcnulty-69a424.netlify.app/']
+
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Origin",'*');
   next();
 });
+
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate))
 //middleware
 app.use(express.json())
 
