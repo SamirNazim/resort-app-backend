bce9212a6baf202419f29a0653afba78fc19853a
Samir Nazim <samirn@Live.ca>
2022-03-14 18:53:14 -0400

    edited server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowlist = ['http://localhost:3000', 'https://serene-mcnulty-69a424.netli
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "content-type");
   next();
   });
 
